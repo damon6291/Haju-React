2cc208f0b4d7dd0efb9c809077c20332a4417c75
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-05-07 00:15:37 -0400

    Updates

---
+++ precache-manifest.a7ae7f43cb6d08bf0974aa3fc26a4979.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddd54b95b98a3f3447a6fdbbdf5d687",
+    "url": "/Haju-React/index.html"
+  },
+  {
+    "revision": "d48c789a25e6ede92fc2",
+    "url": "/Haju-React/static/css/2.6cffe261.chunk.css"
+  },
+  {
+    "revision": "450349e8184532835ceb",
+    "url": "/Haju-React/static/css/main.47f84c0b.chunk.css"
+  },
+  {
+    "revision": "d48c789a25e6ede92fc2",
+    "url": "/Haju-React/static/js/2.5c4e79ee.chunk.js"
+  },
+  {
+    "revision": "00b968f405cef5f5b333d5cbb9675719",
+    "url": "/Haju-React/static/js/2.5c4e79ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450349e8184532835ceb",
+    "url": "/Haju-React/static/js/main.fff8ed1f.chunk.js"
+  },
+  {
+    "revision": "96e1d0b12b3f116a2c3d",
+    "url": "/Haju-React/static/js/runtime-main.8bf7240b.js"
+  },
+  {
+    "revision": "30e166fa7e6f1d06574c4dae48035a77",
+    "url": "/Haju-React/static/media/image1.30e166fa.png"
+  },
+  {
+    "revision": "e9588a3f5f390c210675b958db83cad4",
+    "url": "/Haju-React/static/media/image2.e9588a3f.jpg"
+  },
+  {
+    "revision": "30e166fa7e6f1d06574c4dae48035a77",
+    "url": "/Haju-React/static/media/image3.30e166fa.png"
+  }
+]);
